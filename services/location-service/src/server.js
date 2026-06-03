404e0780582eb5e18e9f5a5a8f29d5791d0d622c
mahitoh <richcalchia@gmail.com>
2026-06-03 13:49:14 -0700

    fix: restore app.listen() in all server.js files

--- services/location-service/src/server.js
+++ services/location-service/src/server.js
@@ -2,3 +2,14 @@ require('dotenv').config();
 const app = require('./app');
 
 const PORT = process.env.PORT || 3005;
+
+const server = app.listen(PORT, () => {
+    console.log(`Location Service running on port ${PORT}`);
+});
+
+const shutdown = () => {
+    server.close(() => process.exit(0));
+};
+
+process.on('SIGTERM', shutdown);
+process.on('SIGINT', shutdown);
